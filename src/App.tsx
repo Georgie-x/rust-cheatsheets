51603ae5d3869657ffa3cfe72d5013d64f0caac8
Georgie <georgiex@gmail.com>
2025-10-09 20:47:44 +0100

    added first four methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<h1>Rust Cheatsheets</h1>
+			<h2>Methods</h2>
+			<h3>map()</h3>
+			<p>Transforms every element</p>
+			<code>
+				{`fn multiply_by_two(vec: Vec<i32>) -> Vec<i32> {
+   vec.into_iter()
+      .map(|x| x * 2)
+      .collect()
+}`}
+			</code>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			<h3>filter()</h3>
+			<p>Transforms each element that matches conditional</p>
+			<code>
+				{`fn only_odds(vec: Vec<i32>) -> Vec<i32> {       
+   vec.into_iter()
+      .filter(|x| x % 2 = 0
+      .collect()
+}`}
+			</code>
+
+			<h3>chars()</h3>
+			<p>Iterates over each character in a string</p>
+			<code>
+				{`fn char_on_new_line() {
+    for character in "hello".chars() {
+        println!("{character}");
+}`}
+			</code>
+
+	<h3>fold()</h3>
+			<p>Reduces a collection into a single value</p>
+			<code>
+				{`fn sum_numbers() {
+    let numbers = [1, 2, 3, 4, 5];
+    let total: i32 = numbers.iter().fold(0, |acc, &x| acc + x);
+        println!("The sum is: {total}");
+}`}
+			</code>
+      
+		</>
+	)
 }
 
 export default App
