ad03c539d1addd275915c4ae9ffdd6ac30c34076
Georgie <georgiex@gmail.com>
2025-10-10 18:58:19 +0100

    added count needs checking

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 		<>
 			<h1>Rust Cheatsheets</h1>
 			<h2>Methods</h2>
-			<h3>map()</h3>
+			<h3>.map()</h3>
 			<p>Transforms every element</p>
 			<code>
 				{`fn multiply_by_two(vec: Vec<i32>) -> Vec<i32> {
@@ -13,7 +13,7 @@ function App() {
 }`}
 			</code>
 
-			<h3>filter()</h3>
+			<h3>.filter()</h3>
 			<p>Transforms each element that matches conditional</p>
 			<code>
 				{`fn only_odds(vec: Vec<i32>) -> Vec<i32> {       
@@ -23,7 +23,7 @@ function App() {
 }`}
 			</code>
 
-			<h3>chars()</h3>
+			<h3>.chars()</h3>
 			<p>Iterates over each character in a string</p>
 			<code>
 				{`fn char_on_new_line() {
@@ -32,13 +32,24 @@ function App() {
 }`}
 			</code>
 
-	<h3>fold()</h3>
+	<h3>.fold()</h3>
 			<p>Reduces a collection into a single value</p>
 			<code>
 				{`fn sum_numbers() {
     let numbers = [1, 2, 3, 4, 5];
     let total: i32 = numbers.iter().fold(0, |acc, &x| acc + x);
         println!("The sum is: {total}");
+}`}
+			</code>
+
+				<h3>.count()</h3>
+			<p>Consumes an iterator and totals the elemnts</p>
+			<code>
+				{`fn how_many_chickens() {
+    let coop = [true, false, false, true, true];
+    	coop.iter
+			.filter(|x| x == true)
+			.count();
 }`}
 			</code>
       
